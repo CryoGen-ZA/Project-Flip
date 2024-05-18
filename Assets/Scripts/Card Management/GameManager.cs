ed9eaeaf810dc24e92b91cd54d7608a763d7ea7b
Michel <michelschnetler@outlook.com>
2024-05-18 17:46:29 +0200

    Added method to align camera's position to the centre of the card layout. Added method to calculate the camera's orthagraphic size based on the card's layout's aspect ratio and sizes

--- Assets/Scripts/Card Management/GameManager.cs
+++ Assets/Scripts/Card Management/GameManager.cs
@@ -6,6 +6,8 @@ namespace Card_Management
     public class GameManager : MonoBehaviour
     {
         private MatchingManager _matchingManager;
+        private Camera _cam;
+        
         public int _rows;
         public int _columns;
         public CardInfoSO cardInfo;
@@ -19,6 +21,37 @@ namespace Card_Management
             
             if (_matchingManager != null)
                 _matchingManager.SetupCards();
+
+            _cam = Camera.main;
+            FitCardLayoutToCameraView(_rows, _columns, cardInfo);
+        }
+
+        private void FitCardLayoutToCameraView(int rows, int columns, CardInfoSO cardInfoSo)
+        {
+            var totalHeight = rows * (cardInfoSo.cardDimensions.y + cardInfoSo.cardPadding.y);
+            var totalWidth = columns * (cardInfoSo.cardDimensions.x + cardInfoSo.cardPadding.x);
+
+            totalWidth += cardInfo.cardPadding.x;
+            totalHeight += cardInfo.cardPadding.y;
+
+            var centrePoint = new Vector3(totalWidth / 2f, -totalHeight / 2f, -1);
+
+            _cam.transform.position = centrePoint;
+            
+            ScaleCameraViewToCardLayout(totalWidth, totalHeight);
+        }
+
+        private void ScaleCameraViewToCardLayout(float totalWidth, float totalHeight)
+        {
+            var cameraHeight = _cam.orthographicSize * 2;
+            var cameraWidth = cameraHeight * _cam.aspect;
+
+            var layoutWidthRatio = totalWidth / cameraWidth;
+            var layoutHeightRatio = totalHeight / cameraHeight;
+            
+            _cam.orthographicSize = layoutWidthRatio < layoutHeightRatio ? 
+                totalHeight / 2 : 
+                totalWidth / 2 / _cam.aspect;
         }
     }
 }
